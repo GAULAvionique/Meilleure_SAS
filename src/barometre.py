9d785b23081a15b43ea666a2ec3c0c44c9c9ce7e
Borhene Harbegue <harbegueborhene12@gmail.com>
2026-07-27 18:44:27 -0400

    Changement adresse

--- src/barometre.py
+++ src/barometre.py
@@ -3,7 +3,7 @@ import board
 import adafruit_bmp280
 
 i2c = board.I2C() #Connecter avec les 2 pins GPIO2 et GPIO3 (SDA et SCL) du raspberry pi 
-bmp = adafruit_bmp280.Adafruit_BMP280_I2C(i2c) #
+bmp = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=0x77) #
 
 while True: 
     try:
